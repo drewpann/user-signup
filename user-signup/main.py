871dbc08ac40a375eed60ae0dc2adc96c584cc48
Drew Pannebecker <drewpann@gmail.com>
2017-04-28 14:10:06 -0500

    It ain't pretty, but it all works. Needs redirect.

--- user-signup/main.py
+++ user-signup/main.py
@@ -16,41 +16,41 @@
 #
 import webapp2
 import cgi
+import re
 
-"""<style>
-    .error {
-        color: red;
-    }
-</style>"""
+V_U = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+V_P = re.compile("^.{3,20}$")
+V_E = re.compile( "^[\S]+@[\S]+.[\S]+$")
+
 title = "<h1>Signup</h1>"
 table = """<form method='post'>
         <table>
             <tr>
                 <td><label for='username'>Username</label></td>
                 <td>
-                    <input name='username' type='text' value='' required>
-                    <span class='error'></span>
+                    <input name='username' type='text' value='%(username)s' required>
+                    <span class='error' style='color:red'>%(nameError)s</span>
                 </td>
             </tr>
             <tr>
                 <td><label for='password'>Password</label></td>
                 <td>
                     <input name='password' type='password' value='' required>
-                    <span class='error'></span>
+                    <span class='error' style='color:red'>%(passError)s</span>
                 </td>
             </tr>
             <tr>
                 <td><label for='verify'>Verify Password</label></td>
                 <td>
                     <input name='verify' type='password' value='' required>
-                    <span class='error'></span>
+                    <span class='error' style='color:red'>%(verifyError)s</span>
                 </td>
             </tr>
             <tr>
                 <td><label for='email'>Email (optional)</label></td>
                 <td>
-                    <input name='email' type='email' value=''>
-                    <span class='error'></span>
+                    <input name='email' type='email' value='%(email)s'>
+                    <span class='error' style='color:red'>%(emailError)s</span>
                 </td>
             </tr>
         </table>
@@ -58,19 +58,53 @@ table = """<form method='post'>
         </form>"""
 content=(title+table)
 
-#def valid_username(username):
+def valid_username(username):
+    return V_U.match(username)
+
+def valid_password(password,verify):
+    if password == verify:
+        return V_P.match(password)
+    else:
+        return False
+
+def valid_email(email):
+    return V_E.match(email)
 
 
 class MainHandler(webapp2.RequestHandler):
+    def write_content(self,nameError='',passError='',verifyError='',emailError='',username='',email=''):
+        self.response.write(content % {'nameError':nameError,'passError':passError,'verifyError':verifyError,
+        'emailError':emailError,'username':username,'email':email})
+
     def get(self):
-        self.response.write(content)
+        self.write_content()
 
     def post(self):
-        username=valid_username(self.request.get('username'))
-        password=valid_password(self.request.get('password','verify'))
+        username=self.request.get('username')
+        email=self.request.get('email')
+        password=self.request.get('password')
+        verify=self.request.get('verify')
 
-        if not(username and password):
-            self.response.out.write(content)
+        ValidName=valid_username(username)
+        ValidPass=valid_password(password,verify)
+        if email != "":
+            ValidEmail=valid_email(email)
+        else:
+            email=True
+
+        nameError=''
+        passError=''
+        verifyError=''
+        emailError=''
+        if not ValidName:
+            nameError="That's not a valid user name"
+        if not ValidPass:
+            passError="That's not a valid password"
+        if password!=verify:
+            verifyError="The passwords do not match"
+        if not ValidEmail:
+            emailError="That's not a valid email address"
+        self.write_content(nameError,passError,verifyError,emailError,username,email)
 
 
 
